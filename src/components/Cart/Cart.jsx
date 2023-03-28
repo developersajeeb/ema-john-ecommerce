4a9fba5cd27705f7131a6be35ad599ff39cf05bf
developersajeeb <sajeebdeveloper@gmail.com>
2023-03-28 17:14:37 +0600

    fix cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,9 +6,16 @@ const Cart = (props) => {
 
     let total = 0;
     let totalShipping = 0
+    let quantity = 0;
     for(const product of cart){
-        total = total + product.price;
+        if(product.quantity === 0){
+            product.quantity = 1;
+        }
+        // product.quantity = product.quantity || 1;
+
+        total = total + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
 
     const tax = total * 7 / 100;
@@ -19,7 +26,7 @@ const Cart = (props) => {
         <div className='sticky top-0 pt-8'>
             <h1 className='text-center font-semibold text-3xl'>Order Summary</h1>
             <div className='mx-10 mt-12 grid gap-6 border-2 border-gray-500 rounded-lg p-4'>
-                <p className='text-xl'>Selected Items: {cart.length}</p>
+                <p className='text-xl'>Selected Items: {quantity}</p>
                 <p className='text-xl'>Total Price: ${total}</p>
                 <p className='text-xl'>Total Shipping Charge: ${totalShipping}</p>
                 <p className='text-xl'>Tax: ${tax.toFixed(2)}</p>
